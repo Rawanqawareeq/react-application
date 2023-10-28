c0c2141b700d0b5ebb1d0c4f9225f5c1fe6157c5
Rawanqawareeq <rawanqawareeq95@gmail.com>
2023-10-28 14:40:57 +0200

    first project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './component/navbar/Navbar'
+import Products from './component/products/Products'
+import Footer from './component/footer/Footer'
+
+export default function App() {
+  return (
+    <div>
+     
+      <Navbar/>
+      <Products/>
+      <Footer/>
+    </div>
+  )
+}
---
+++ src/component/products/Products.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Product from '../product/product'
+
+export default function Products() {
+    const producrs =[
+        {id:1, title:'product one', desc:'this in product one' , price:20000},
+        {id:2, title:'product two', desc:'this in product two' , price:20000},
+        {id:3, title:'product three', desc:'this in product three', price:20000 },
+        {id:4, title:'product four', desc:'this in product four', price:20000 }
+        
+    ]
+  return (
+    
+        
+    
+    <div className='row'>
+        {producrs.map((product)=>
+           <Product {...product} key={product.id}/>
+
+        
+       
+
+        )}
+       
+        
+     
+    </div>
+  )
+}
